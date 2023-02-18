ec79431dfe9c98ac7a290b0e00ece6e7bf6c38f7
Gouiaa, Ramy <ramy.gouiaa@hs-ruhrwest.de>
2023-02-18 20:00:52 +0100

    adding identity generation adding welcome message to the default route

--- encryptionServer.js
+++ encryptionServer.js
@@ -19,6 +19,15 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  try {
+    res.send( 'Welcome te the Ethereum encryption service, you can create an account using metamask or simply go to /identity to get new Ethereum identity.Now Enjoy exchanging secret messages with your friends' );
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('server error!');
+  }
+});
+
 // POST route to encrypt message
 app.post('/encrypt', async (req, res) => {
   try {
@@ -42,5 +51,20 @@ app.post('/decrypt', async (req, res) => {
     res.status(500).send('Error decrypting message');
   }
 });
+
+app.get('/identity', (req, res) => {
+  try {
+    //const { privateKey, encryptedMessage } = req.body;
+    //const decryptedMessage = await EthCrypto.decryptWithPrivateKey(privateKey, EthCrypto.cipher.parse(encryptedMessage));
+    const identity = EthCrypto.createIdentity();
+    res.send( identity );
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('could not create account!');
+  }
+
+
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
